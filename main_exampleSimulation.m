e2998a66269a3b28e654d37ae5ce8f98cc6141e0
matgrand <matteo.grandin@gmail.com>
2025-03-17 18:47:50 +0100

    +

--- main_exampleSimulation.m
+++ main_exampleSimulation.m
@@ -9,71 +9,57 @@ addpath('Sources')
 %% Step 0: Load Current Execise Data 
 %         (needed to run the simulation)
 %--------------------------------------------------------------------%
-    currentExercise=1;
-    
-    % Possible simplifications (activate by setting the variable to 1):
-    removeContraints=0;
-    
-    exerciseData=LoadMPCExercise(currentExercise,removeContraints);
-    
-    % Relavant data for the controller design
-    exerciseData.u_max
-    exerciseData.u_min
+currentExercise=1;
+
+% Possible simplifications (activate by setting the variable to 1):
+removeContraints=0;
+
+exerciseData=LoadMPCExercise(currentExercise,removeContraints);
+
+% Relavant data for the controller design
+exerciseData.u_max
+exerciseData.u_min
 
 %-----------------------------%
 %%   MPC controller design   %%
 %-----------------------------%
-    %% Step 1: upload a model of the system to be controlled
-        % Possible simplifications (activate by setting the variable to 1):
-        exactModel=1;
-        SystemModel=LoadSystemModel(currentExercise,exactModel);
-        
-    %% Step 2: use to model to create the condensed MPC matrices
-    % You are provided with the command
-    %    condensedMatrices=BuildCondendsedMPCmatrices(SystemModel,R,Q,F,f,PH); % Warning: this command will produce an error, since R,Q,F,f and PH are currently undefined.
-
-%-----------------------------%
-%%   Closed loop simualtion  %%
-%-----------------------------%
-%------------%
-% %% Option 1: Using Simulink:
-% %------------%
-%     sim('MPC_exactState_noDistrubance')
-%     plot(SystemOutput.time, SystemOutput.signals.values)
-% %------------%
+%% Step 1: upload a model of the system to be controlled
+    % Possible simplifications (activate by setting the variable to 1):
+    exactModel=1;
+    SystemModel=LoadSystemModel(currentExercise,exactModel);
+    
+%% Step 2: use to model to create the condensed MPC matrices
+% You are provided with the command
+%    condensedMatrices=BuildCondendsedMPCmatrices(SystemModel,R,Q,F,f,PH); % Warning: this command will produce an error, since R,Q,F,f and PH are currently undefined.
 
 %------------%
 %% Option 2: Using a matlab for cycle:
 %------------%
 % Initalization
-    x_k=zeros(size(exerciseData.System.A,1),1);
+x_k=zeros(size(exerciseData.System.A,1),1);
 
-    u_memory=0;
-    x_memory=x_k;
-    y_memory=exerciseData.System.C*x_k;
+u_memory=0;
+x_memory=x_k;
+y_memory=exerciseData.System.C*x_k;
 
 % Basic Closed-Loop system simulation
-    for k=exerciseData.Ts:exerciseData.Ts:exerciseData.SimTime
+for k=exerciseData.Ts:exerciseData.Ts:exerciseData.SimTime
 
-        % Compute the the control action to be used using MPC
-        u_k=1000; % in this example always u=1     
+    % Compute the the control action to be used using MPC
+    u_k=1000; % in this example always u=1     
+
+    % Simulate Plant Response
+    x_kplus1=exerciseData.System.A*x_k+exerciseData.System.B*u_k; 
+    y=exerciseData.System.C*x_k+exerciseData.System.D*u_k;
     
-        % Simulate Plant Response
-        x_kplus1=exerciseData.System.A*x_k+exerciseData.System.B*u_k; 
-        y=exerciseData.System.C*x_k+exerciseData.System.D*u_k;
-        
-        % Save for plots
-        u_memory(:,end+1)=u_k;
-        x_memory(:,end+1)=x_k;
-        y_memory(:,end+1)=y;
-    
-        % Update and repeat    
-        x_k=x_kplus1;
-    end
+    % Save for plots
+    u_memory(:,end+1)=u_k;
+    x_memory(:,end+1)=x_k;
+    y_memory(:,end+1)=y;
 
-% write a print statement
-
-
+    % Update and repeat    
+    x_k=x_kplus1;
+end
 
 % Basic Plot
 figure
